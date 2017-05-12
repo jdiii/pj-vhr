40991da25e80194ff721574f5e4c0467ca17afc4
johndavey <john.n.davey@gmail.com>
2017-05-12 13:44:15 -0400

    Init

---
+++ app.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var request = require('request');
+var config = require('./config');
+var bodyParser = require('body-parser');
+var app = express();
+app.use(bodyParser.json());
+
+app.get('/', function(req, res){
+	res.send('There\'s nothing here!');
+});
+
+app.get('/vhr/:vin', function(req, res) {
+
+	console.log(req.params.vin);
+
+	var form = {
+		'SID': config.autocheckSid,
+		'PWD': config.autocheckPwd,
+		'CID': config.authocheckCid,
+		'VIN': req.params.vin
+	};
+
+	request.post({url: config.autocheckHostname + config.autocheckApiPath, form: form}, function(err, response, body){
+		console.log(response);
+
+		res.send(response);
+	});
+
+});
+
+
+app.listen(3001);
+console.log('Listening on port 3001...');
---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+
+	/* Autocheck user variables */
+	'autocheckSid' : '',
+	'autocheckCid' : '',
+	'autocheckPwd' : '',
+
+	/* Autocheck API path */
+	'autocheckHostname' : 'http://www.autocheck.com/',
+	'autocheckApiPath' : 'DealerWebLink.jsp'
+
+}
