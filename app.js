5b97e22e44b616e1635db29ca2f1fd52c20d1fe7
john davey <john.n.davey@gmail.com>
2017-05-15 13:57:05 +0000

    Copy objects semi-properly and fix typos

--- app.js
+++ app.js
@@ -5,23 +5,35 @@ var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.json());
 
+var form = {
+	'SID': config.autocheckSid,
+	'PWD': config.autocheckPwd,
+	'CID': config.authocheckCid
+};
+
 app.get('/', function(req, res){
 	res.send('There\'s nothing here!');
 });
 
 app.get('/vhr/:vin', function(req, res) {
-	var form = {
-		'SID': config.autocheckSid,
-		'PWD': config.autocheckPwd,
-		'CID': config.authocheckCid,
-		'VIN': req.params.vin
-	};
-	request.post({url: config.autocheckHostname + config.autocheckApiPath, form: form}, function(err, response, body){
+	var vhrForm = JSON.parse(JSON.stringify(form));
+	vhrForm['VIN'] = req.params.vin;
+	request.post({url: config.autocheckHostname + config.autocheckApiPath, form: vhrForm}, function(err, response, body){
 		res.send(response);
 	});
+});
 
+app.get('/badge/:vin', function(req, res) {
+
+	var badgeForm = JSON.parse(JSON.stringify(form));
+	badgeForm['VIN'] = req.params.vin;
+	badgeForm['REPORT'] = 'R';
+	badgeForm['TEMPLATE'] = '2'; //template 1 is 300x138 px, template 2 is 180x82 px
+	request.post({url: config.autocheckHostname + config.autocheckApiPath, form: badgeForm}, function(err, response, body){
+		res.send(response);
+	});
 });
 
 
-app.listen(3001);
-console.log('Listening on port 3001...');
+app.listen(8080);
+console.log('Listening on port 8080...');
